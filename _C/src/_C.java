5c9e98732fb34a1b6e4433e8ab0cf94c3534231d
Eugene Pugachev <eugene.pugachev@gmail.com>
2018-11-20 16:53:55 +0300

    add test "test"(2), _C(1)

--- _C/src/_C.java
+++ _C/src/_C.java
@@ -1,7 +1,7 @@
 public class _C {
     private static int $;
     public static void main(String[] main) {
-        String a_b;
+        String a_b="123";
         System.out.print($);
         System.out.print(a_b);
     }
