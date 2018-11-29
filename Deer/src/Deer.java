323a4042ee6814badbb30c7bfbbbee79638ed370
Eugene Pugachev <eugene.pugachev@gmail.com>
2018-11-29 23:08:32 +0300

    Derr

---
+++ Deer/src/Deer.java
@@ -0,0 +1,24 @@
+public class Deer {
+    public Deer() {
+        System.out.print("Deer_");
+    }
+    public Deer(int age) {
+        System.out.print("DeerAge");
+    }
+    private boolean hasHorns() {
+        return false;
+    }
+    public static void main(String[] args) {
+        Deer deer = new Reindeer(5);
+        System.out.println(","+deer.hasHorns());
+    }
+}
+class Reindeer extends Deer {
+    public Reindeer(int age) {
+        //super(age);
+        System.out.print("Reindeer");
+    }
+    public boolean hasHorns() {
+        return true;
+    }
+}
